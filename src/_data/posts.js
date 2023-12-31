b106a06bda732a2c8043d8cdde19f2989d06e875
jonathan youngblood <jonathan.youngblood@gmail.com>
2023-12-31 12:14:53 -0600

    ex nihilo or whatever

---
+++ src/_data/posts.js
@@ -0,0 +1,10 @@
+const fetch = require("node-fetch")
+
+// Fetch the most recent 10 posts
+// If we want more, we can loop through requests, up to 100 at a time, and append them to an array
+// https://developer.wordpress.org/rest-api/using-the-rest-api/pagination/
+
+module.exports = async () => {
+  const res = await fetch("https://printshop.hxgf.io/wp-json/wp/v2/posts?categories=5")
+  return await res.json()
+}
