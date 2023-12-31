01df2e05bace6c7cf1f42043733ed505e636c70a
jonathan youngblood <jonathan.youngblood@gmail.com>
2023-12-31 14:26:12 -0600

    ok i feel good about this now <3

--- src/_data/articles.js
+++ src/_data/articles.js
@@ -5,6 +5,8 @@ const fetch = require("node-fetch")
 // https://developer.wordpress.org/rest-api/using-the-rest-api/pagination/
 
 module.exports = async () => {
-  const res = await fetch("https://printshop.hxgf.io/wp-json/wp/v2/posts?categories=5")
-  return await res.json()
+  const res = await fetch("https://printshop.hxgf.io/wp-json/wp/v2/posts?categories=5");
+  let content = await res.json();
+  // console.log(content);
+  return content;
 }
