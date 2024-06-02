e897fcc1953d388a41d231265107d864a9bc18e5
jonathan youngblood <jonathan.youngblood@gmail.com>
2024-06-02 16:00:07 -0500

    projects table

---
+++ src/_data/projects.js
@@ -0,0 +1,853 @@
+
+module.exports = async () => {
+
+
+
+				let projects = [
+
+// fixit maybe				
+// just show the favorites at first
+	// then have a button that's like SHOW ME ALL OF THEM				
+
+// put everything in these categories:
+/*
+  - Current (active/recent) projects (visible)
+  - Old projects / archive
+  - Could-be projects (in the works, eventual/potential*)
+  - Portfolio (hidden, able to show it off to people who need to see  (optional client services))
+*/
+
+
+
+				
+				
+				{
+					title: "STEREO",
+					url: "https://stereotk.com/",
+					description: "Brutalist web dev tool kit for quickly building sites and APIs",
+//					 -- we've had to build A LOT of the same kinds of things over and over over the past decade or so, and this framework has slowly developed to make things easier. now that it's solidified as a tool, we're putting effort into writing documentation and making it easy for people to learn so that they, too, may benefit from these solutions. it's like the easiest way to make web sites with php!
+
+// primitive/generic - simplified express concept for presentation (user-facing) web application layer - designed to make routing and display a breeze
+
+					year: "2009-",
+					type: "OSS",
+					status: "Active",
+					icon: "./images/stereo-headphones-black-big.png",
+				},
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				{
+					title: "cassett.es",
+					url: "https://cassett.es/",
+					description: "Mix tapes of songs we like. Used to be a podcast, but there was just never enough time to do it consistently. There will be new mixes one day (already have a few in the can, just not posted). Site redesigned in 2021.",
+					year: "2013-2015",
+					type: "",
+					status: "Archived",
+/*
+<a href="https://cassett.es/"><img class="w3" src="/images/cassettes-tape.svg" /></a> &nbsp;
+<a class="near-black underline-title" href="https://cassett.es/">cassett.es</a><br /><br /> -- 
+mix tapes, made with love, and dropped an rss feed!. 
+
+this series has been a labor of love, released in "seasons", updated occasionally (as life permits), haven't released
+anything in a while, but we're bringing it back as a youtube series and making new episodes in 2021!<br /><br />
+
+just started posting cool songs to twitter, <a href="https://twitter.com/cassett_es" target="_blank">check it</a>
+*/
+
+				},
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				{
+					title: "darkwave",
+					url: "https://darkwave.ltd/",
+					description: " \"full-stack\" framework for building practical data-driven web applications (it's like wordpress but for web apps)",
+/*
+
+<a href="https://darkwave.ltd/"><img class="w3" src="/images/dw-triangles-transparent.png" /></a> &nbsp;
+<a class="near-black underline-title" href="https://darkwave.ltd/">darkwave</a><br /><br /> --       brutalist user-friendly content management + design patterns. 
+      [see also: extendable admin, workflow, scaffolding, boilerplate]. 
+      for quickly developing complex admin-driven web applications. 
+      
+      we've had to write a lot of these things where no appropriate solution existed (available solutions were "too much")
+
+specialized functionality/components/patterns for building complex web applications quickly -- toolkit for building custom admins and the sites they power
+*/
+
+					year: "2013-",
+					type: "OSS",
+					status: "Active",
+					icon: "./images/dw-triangles-transparent.png",
+				},
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				{
+					title: "DVST",
+					url: "https://dvst.cc/",
+					description: "kinda like a web zine? place where i'm putting links of all the interesting stuff i find",
+
+/*
+				dvst
+
+						      <div class="f4 fw4 mb2"><a class="near-black underline-title" href="https://dvst.cc" target="_blank">DVST</a></div>
+		      -- we've been putting together a webzine for a long time, there's just never any time to finish it...it's been a long year, ok? putting it together out in the open because who cares, right?<Br /><br />		
+
+<a href="https://dvst.cc/"><img class="w3" src="" /></a> &nbsp;
+<a class="near-black underline-title" href="https://dvst.cc/">dvst</a><br /><br /> -- PUBLIC WORK IN PROGRESS putting together a webzine, out in the open. it's about art and technology and society -- "internet arts and culture magazine" -- cyber world magazine<br /><br /><br /><br />
+
+*/
+
+					year: "2018-",
+					status: "Active",
+					icon: "./images/dvst-thin-transparent.png",
+				},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				{
+					title: "uiuxui",
+					url: "https://uiuxui.tumblr.com/",
+// 					-- shitposting on tumblr about ux. going to bring this back as its own web site one day?
+					description: " ",
+					year: " ",
+					type: " ",
+					status: " ",
+				},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				{
+					title: "ffffonts",
+					url: "https://ffffonts.tumblr.com/",
+					description: "jon gold called this site 'incredible'",
+// 					-- look at all these cool fonts!
+					year: " ",
+					type: " ",
+					status: " ",
+				},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				{
+					title: "css pentagram",
+					url: "https://csspentagram.hxgf.io/",
+					description: "it's a pentagram. made with css. i used to have the domain csspentagram.com, but no mas...",
+					year: " ",
+					type: " ",
+					status: " ",
+				},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				{
+					title: "analog music publishing",
+					url: "http://analog.hxgf.io/",
+					description: " ",
+					year: " ",
+					type: " ",
+					status: " ",
+				},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				{
+					title: "field recordings",
+					url: "http://fieldrecordings.hxgf.io/",
+					description: " ",
+					year: " ",
+					type: " ",
+					status: " ",
+				},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				
+
+
+				{
+					title: "smokeJS",
+					url: "https://smoke.js.org",
+					description: " ",
+// a modern classic: framework-agnostic styled alert system for javascript. ★ 936 github stars! ★
+					year: " ",
+					type: " ",
+					status: " ",
+				},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				{
+					title: "scratch",
+					url: "https://scratch.hxgf.io",
+					description: " ",
+// html boilerplate + css/js primitives (tachyons + alpine)
+					year: " ",
+					type: " ",
+					status: " ",
+				},
+
+
+
+
+
+
+
+				{
+					title: "scanlines",
+					url: "https://scanlines.hxgf.io",
+					description: " ",
+					year: " ",
+					type: " ",
+					status: " ",
+				},
+
+
+
+
+
+				{
+					title: "ucqb",
+					url: "https://ucqb.hxgf.io",
+					description: " ",
+					year: " ",
+					type: " ",
+					status: " ",
+				},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 				{
+// 					title: "lexxi",
+// 					url: "https://",
+// // 					https://github.com/jyoungblood/lexxi
+// // https://github.com/jyoungblood/lexxi-utilities
+// // https://github.com/jyoungblood/lexxi-handlebars
+// 					description: " ",
+// 					year: " ",
+// 					type: " ",
+// 					status: " ",
+// // 				},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				{
+					title: "fashion-camp",
+					url: "https://github.com/jyoungblood/fashion-camp",
+					description: "fashionable .seestyle themes for coda",
+					year: " ",
+					type: " ",
+					status: " ",
+				},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				{
+					title: "base16-chalk-dark",
+					url: "https://github.atom.io/themes/base16-chalk-dark",
+					description: "port of the textmate theme for atom (1684 downloads)",
+					year: " ",
+					type: " ",
+					status: " ",
+				},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				{
+					title: "tigershark!",
+					url: "https://github.com/hxgf/tigershark",
+					description: " ",
+					year: " ",
+					type: " ",
+					status: " ",
+				},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				{
+					title: "JY",
+					url: "https://jy.hxgf.io",
+					description: " ",
+					year: " ",
+					type: " ",
+					status: " ",
+				},
+
+
+
+
+
+
+
+
+
+
+				{
+					title: "JY.com",
+					url: "https://jonathanyoungblood.com",
+					description: " ",
+					year: " ",
+					type: " ",
+					status: " ",
+				},
+
+
+
+
+				{
+					title: "hyperlocker",
+					url: "https://hyperlocker.hxgf.io",
+					description: " ",
+					year: " ",
+					type: " ",
+					status: " ",
+				},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				{
+					title: "xerOS",
+					url: "https://xeros.hxgf.io",
+					description: " ",
+					year: " ",
+					type: " ",
+					status: " ",
+				},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				{
+					title: "the novelist",
+// https://web.archive.org/web/2013*/thenovelist.cc
+					description: " ",
+					year: " ",
+					type: " ",
+					status: " ",
+				},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				{
+					title: "cut/copy",
+// https://web.archive.org/web/20130721020901/http://cutcopy.cc/
+					description: " ",
+					year: " ",
+					type: " ",
+					status: " ",
+				},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				{
+					title: "ssssnakes",
+// http://web.archive.org/web/2019*/ssssnakes.com
+					description: " ",
+					year: " ",
+					type: " ",
+					status: " ",
+				},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				{
+					title: "ARTFA.GS",
+// https://web.archive.org/web/*/artfa.gs
+					description: " ",
+					year: "2008-2011",
+					type: " ",
+					status: " ",
+				},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				{
+					title: "YOU FAIL THE INTERNET",
+// https://web.archive.org/web/*/youfailtheinter.net
+					description: " ",
+					year: " ",
+					type: " ",
+					status: " ",
+				},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				{
+					title: "ljbf.o",
+// https://web.archive.org/web/*/letsjustbefriends.org
+					description: " ",
+					year: "2004-2009",
+					type: " ",
+					status: " ",
+				},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				{
+					title: "327.com",
+// https://web.archive.org/web/*/327.com
+					description: "The site that started it all. An occasionally-updated web-zine w/ album reviews, photo features, editorials, interviews...really just whatever we felt like. It was also home to web sites for several of my friends. I sold the domain in 2009 and still regret that decision to this day ¯\_(ツ)_/¯",
+					year: "1998-2009",
+					type: " ",
+					status: " ",
+				},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+				{
+					title: "",
+					url: "https://",
+					description: "",
+					year: "",
+					type: "",
+					status: "",
+					era: ""
+				},
+*/
+				
+				];
+
+			
+			
+			/*
+			
+
+In memoriam:         
+
+e society
+florida
+j.327
+
+fontaine
+ssats
+tgs
+https://web.archive.org/web/20051227110201/http://www.thirdgradescuffle.com/
+
+unleaded multimedia
+ceiling tiles?
+
+				hxgf old designs
+				old versions of jy.com? (/etc/hustlin.sh)
+			
+				personal dashboard
+				static city
+				cassettes admin
+				lips admin, styleguide
+				
+				misc scrap @ dribbble
+			
+				aesthetic/inspo/lookbook @ arena
+				
+				??
+				oriax
+				apub
+				
+			*/
+			
+			/*
+				??
+				flaming lips
+				changelog
+				jcdb
+				ocean mst
+				nrha
+				owl cam
+				warpaint
+				loyal order
+
+flinchbaugh
+mubt
+fsbcdc
+redcrown
+
+
+geocities 4582?
+Angelfire roakdill?
+Tripod site?
+list site?
+
+			*/
+			
+
+      
+// fixit disabling for now
+
+				// $(function(){
+
+        //   // projects.map(function (project) {
+				// 	$.each(projects, function(i, project){
+				// 		$('[data-id="project-table"]').append(`
+				// 			<tr>
+				// 				<td style="width: 20%;">
+				// 					<a class="black fw6 no-underline" href="${project.url}" target="_blank">${project.title}</a>
+				// 				</td>
+				// 				<td style="width: 50%;" class="lh-copy">
+				// 					${typeof project.icon === 'undefined' ? `` : `<img class="w1" src="${project.icon}" />`}
+				// 					${project.description}
+				// 				</td>
+				// 				<td style="width: 5%;" class="f7 black-80">
+				// 					${project.year}
+				// 				</td>
+				// 				<td style="width: 5%;" class="f7 black-80">
+				// 					${project.type}
+				// 				</td>
+				// 				<td style="width: 5%;" class="f7 black-80">
+				// 					${project.era}
+				// 				</td>
+				// 				<td style="width: 5%;" class="f7 black-80">
+				// 					${project.status}
+				// 				</td>
+				// 			</tr>
+				// 		`);
+				// 	});
+				// 	$('[data-id="project-table"] tr').on('click', function(){
+				// 		window.open($(this).find('a').attr('href'), '_blank');
+				// 	});
+				// });
+
+
+
+
+  console.log(projects);
+  return projects;
+
+
+}
