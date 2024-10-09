fa4e33ff9b79eeb283916c42f60a7c2972802396
jonathan youngblood <jonathan.youngblood@gmail.com>
2024-10-09 16:51:40 -0500

    Updates - 2024-10-09 16:51:35

---
+++ src/data/projects.js
@@ -0,0 +1,837 @@
+export const projects = [
+
+
+
+  // {
+  //   title: "",
+  //   url: "",
+  //   description: "",
+  //   tags: [],
+  //   year: "",
+  //   status: "",
+  // },
+
+
+
+
+  {
+    title: "Stereo Internet Toolkit",
+    url: "https://stereotk.com/",
+    description: "Full-stack web development framework for quickly building sites, APIs, and applications",
+    tags: ["oss"],
+    portfolio: true,
+    year: "2010-",
+    status: "Current",
+  },
+
+
+  
+
+  {
+    title: "VPHP",
+    url: "https://vphp.dev/",
+    description: "Vanilla PHP packages with no dependencies",
+    tags: ["oss"],
+    portfolio: true,
+    year: "2023-",
+    status: "Ongoing",
+  },
+
+
+  
+
+
+  {
+    title: "JY OSS",
+    url: "https://jyoungblood.github.io/",
+    description: "Open source project catalog",
+    tags: ["oss"],
+    year: "2024",
+    status: "Ongoing",
+  },
+
+
+
+
+  
+  {
+    title: "Darkwave",
+    url: "https://darkwave.ltd/",
+    description: "Pragmatic application runtime",
+    tags: ["oss"],
+    year: "2017-",
+    status: "Ongoing",
+  },
+
+
+  
+
+  
+  {
+    title: "Pocket Docs",
+    url: "https://jyoungblood.github.io/pocketdocs/",
+    description: "Attractive template for writing single-page documentation with semantic HTML",
+    portfolio: true,
+    tags: ["oss"],
+    year: "2024",
+    status: "Complete",
+  },
+
+
+  
+
+
+  {
+    title: "Slime",
+    url: "https://slime.technology/",
+    description: "Starter kit for building web apps with Slim 4 with Handlebars templates",
+    tags: ["oss"],
+    year: "2022",
+    status: "Complete",
+  },
+
+
+  
+
+
+
+
+  {
+    title: "jQuery Digital Sign",
+    url: "https://github.com/jyoungblood/jquery-digital-sign",
+    description: "Simple digital signage solution built with jQuery and Tachyons",
+    tags: ["oss"],
+    portfolio: true,
+    year: "2023",
+    status: "Complete",
+  },
+
+
+  {
+    title: "Smoke.js",
+    url: "https://smoke.js.org/",
+    description: "Framework-agnostic styled alert system for Javascript",
+    tags: ["oss"],
+    portfolio: true,
+    year: "2011",
+    status: "Complete",
+  },
+
+  
+
+
+
+  {
+    title: "Scratch",
+    url: "https://github.com/jyoungblood/scratch",
+    description: "",
+    tags: ["oss"],
+    hidden: true,
+    year: "",
+    status: "Complete",
+  },
+
+
+  
+
+  {
+    title: "0x00",
+    url: "https://github.com/jyoungblood/0x00",
+    description: "",
+    tags: ["oss"],
+    hidden: true,
+    year: "",
+    status: "Complete",
+  },
+
+
+  
+
+
+
+  {
+    title: "Analog Music",
+    url: "https://analog-music.com/",
+    description: "Record label for music projects from me and my friends",
+    tags: ["sound"],
+    year: "1999-",
+    status: "Upcoming",
+  },
+
+
+
+
+
+  {
+    title: "DVST",
+    url: "https://dvst.cc/",
+    description: "Aesthetic digest, internet scrapbook",
+    tags: ["publication"],
+    year: "2020-",
+    status: "Upcoming",
+  },
+
+
+  
+
+
+  {
+    title: "Antipatterns",
+    url: "https://antipatterns.tv/",
+    description: "Web zine for developers",
+    tags: ["publication"],
+    year: "2024",
+    status: "Upcoming",
+  },
+
+
+  
+
+  {
+    title: "Field Recordings",
+    url: "https://field-recordings.com/",
+    description: "Collection of ambient soundscapes",
+    tags: ["sound"],
+    year: "2015-",
+    status: "Upcoming",
+  },
+
+
+  
+
+
+  
+
+  {
+    title: "UCQB",
+    url: "https://ucqb.org/",
+    description: "Universal Church of Quantum Benevolence",
+    tags: ["social"],
+    year: "2024",
+    status: "Upcoming",
+  },
+
+
+  
+
+  {
+    title: "SCNLNS",
+    url: "https://scnlns.com/",
+    description: "Current music project",
+    tags: ["sound"],
+    year: "2019-",
+    status: "Upcoming",
+  },
+
+
+  
+
+
+  {
+    title: "HXGF.IO",
+    url: "https://hxgf.io/",
+    description: "",
+    tags: [],
+    hidden: true,
+    year: "",
+    status: "Ongoing",
+  },
+
+
+  
+
+
+
+
+
+
+
+
+  {
+    title: "Ocean Companies MST",
+    url: "https://port164.com/",
+    description: "",
+    tags: ["client"],
+    portfolio: true,
+    year: "2018-",
+    status: "Ongoing",
+  },
+
+
+  
+
+
+
+  {
+    title: "BLU",
+    url: "https://blu.oceancompaniesok.com/",
+    description: "",
+    tags: ["client"],
+    portfolio: true,
+    year: "",
+    status: "Ongoing",
+  },
+
+
+
+
+
+
+  {
+    title: "NRHA",
+    url: "https://nrha.com/",
+    description: "",
+    tags: ["client"],
+    portfolio: true,
+    year: "",
+    status: "Ongoing",
+  },
+
+
+
+
+
+  {
+    title: "Stallion Manager",
+    url: "https://stallionmanager.equipost.com/",
+    description: "",
+    tags: ["client"],
+    portfolio: true,
+    year: "",
+    status: "Ongoing",
+  },
+
+
+
+
+
+  {
+    title: "CSS PENTAGRAM",
+    url: "https://csspentagram.com/",
+    description: "It's a pentagram. Made with CSS.",
+    tags: [],
+    hidden: true,
+    year: "2014",
+    // status: "Complete",
+    status: "Complete",
+  },
+
+
+
+
+
+		// 		{
+		// 			title: "STEREO",
+		// 			url: "https://stereotk.com/",
+		// 			description: "Brutalist web dev tool kit for quickly building sites and APIs .. we've had to build A LOT of the same kinds of things over and over over the past decade or so, and this framework has slowly developed to make things easier. now that it's solidified as a tool, we're putting effort into writing documentation and making it easy for people to learn so that they, too, may benefit from these solutions. it's like the easiest way to make web sites with php! .. primitive/generic - simplified express concept for presentation (user-facing) web application layer - designed to make routing and display a breeze",
+		// 			tags: [ ],
+    // year: "2009-",
+		// 			type: "oss",
+		// 			status: "Active",
+		// 			icon: "./images/stereo-headphones-black-big.png",
+		// 		},
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+		// 		{
+		// 			title: "cassett.es",
+		// 			url: "https://cassett.es/",
+		// 			description: "Mix tapes of songs we like. Used to be a podcast, but there was just never enough time to do it consistently. There will be new mixes one day (already have a few in the can, just not posted). Site redesigned in 2021.",
+		// 			tags: [ ],
+    // year: "2013-2015",
+		// 			type: "",
+		// 			status: "Archived",
+
+		// 		},
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+		// 		{
+		// 			title: "darkwave",
+		// 			url: "https://darkwave.ltd/",
+		// 			description: " \"full-stack\" framework for building practical data-driven web applications (it's like wordpress but for web apps)",
+
+		// 			tags: [ ],
+    // year: "2013-",
+		// 			type: "oss",
+		// 			status: "Active",
+		// 			icon: "./images/dw-triangles-transparent.png",
+		// 		},
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+		// 		{
+		// 			title: "DVST",
+		// 			url: "https://dvst.cc/",
+		// 			description: "kinda like a web zine? place where i'm putting links of all the interesting stuff i find",
+
+
+		// 			tags: [ ],
+    // year: "2018-",
+		// 			status: "Active",
+		// 			icon: "./images/dvst-thin-transparent.png",
+		// 		},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// 		{
+		// 			title: "uiuxui",
+		// 			url: "https://uiuxui.tumblr.com/",
+		// 			description: " ",
+		// 			tags: [ ],
+    // year: " ",
+		// 			type: " ",
+		// 			status: " ",
+		// 		},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// 		{
+		// 			title: "ffffonts",
+		// 			url: "https://ffffonts.tumblr.com/",
+		// 			description: "look at all these cool fonts! jem gold once called this site 'incredible'",
+		// 			tags: [ ],
+    // year: " ",
+		// 			type: " ",
+		// 			status: " ",
+		// 		},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// 		{
+		// 			title: "css pentagram",
+		// 			url: "https://csspentagram.hxgf.io/",
+		// 			description: "it's a pentagram. made with css. i used to have the domain csspentagram.com, but no mas...",
+		// 			tags: [ ],
+    // year: " ",
+		// 			type: " ",
+		// 			status: " ",
+		// 		},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// 		{
+		// 			title: "analog music publishing",
+		// 			url: "http://analog.hxgf.io/",
+		// 			description: " ",
+		// 			tags: [ ],
+    // year: " ",
+		// 			type: " ",
+		// 			status: " ",
+		// 		},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// 		{
+		// 			title: "field recordings",
+		// 			url: "http://fieldrecordings.hxgf.io/",
+		// 			description: " ",
+		// 			tags: [ ],
+    // year: " ",
+		// 			type: " ",
+		// 			status: " ",
+		// 		},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				
+
+
+		// 		{
+		// 			title: "smokeJS",
+		// 			url: "https://smoke.js.org",
+		// 			description: "a modern classic: framework-agnostic styled alert system for javascript. ★ 936 github stars! ★",
+		// 			tags: [ ],
+    // year: " ",
+		// 			type: " ",
+		// 			status: " ",
+		// 		},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// 		{
+		// 			title: "scratch",
+		// 			url: "https://scratch.hxgf.io",
+		// 			description: "html boilerplate + css/js primitives (tachyons + alpine)",
+		// 			tags: [ ],
+    // year: " ",
+		// 			type: " ",
+		// 			status: " ",
+		// 		},
+
+
+
+
+
+
+
+		// 		{
+		// 			title: "scanlines",
+		// 			url: "https://scanlines.hxgf.io",
+		// 			description: " ",
+		// 			tags: [ ],
+    // year: " ",
+		// 			type: " ",
+		// 			status: " ",
+		// 		},
+
+
+
+
+
+		// 		{
+		// 			title: "ucqb",
+		// 			url: "https://ucqb.hxgf.io",
+		// 			description: " ",
+		// 			tags: [ ],
+    // year: " ",
+		// 			type: " ",
+		// 			status: " ",
+		// 		},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// 		{
+		// 			title: "the novelist",
+    //       url: "https://web.archive.org/web/2013*/thenovelist.cc",
+		// 			description: " ",
+		// 			tags: [ ],
+    // year: " ",
+		// 			type: " ",
+		// 			status: " ",
+		// 		},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// 		{
+		// 			title: "cut/copy",
+    //       url: "https://web.archive.org/web/20130721020901/http://cutcopy.cc/",
+		// 			description: " ",
+		// 			tags: [ ],
+    // year: " ",
+		// 			type: " ",
+		// 			status: " ",
+		// 		},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// 		{
+		// 			title: "ssssnakes",
+    //       url: "http://web.archive.org/web/2019*/ssssnakes.com",
+		// 			description: " ",
+		// 			tags: [ ],
+    // year: " ",
+		// 			type: " ",
+		// 			status: " ",
+		// 		},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// 		{
+		// 			title: "ARTFA.GS",
+    //       url: "https://web.archive.org/web/*/artfa.gs",
+		// 			description: " ",
+		// 			tags: [ ],
+    // year: "2008-2011",
+		// 			type: " ",
+		// 			status: " ",
+		// 		},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// 		{
+		// 			title: "YOU FAIL THE INTERNET",
+    //       url: "https://web.archive.org/web/*/youfailtheinter.net",
+		// 			description: " ",
+		// 			tags: [ ],
+    // year: " ",
+		// 			type: " ",
+		// 			status: " ",
+		// 		},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// 		{
+		// 			title: "ljbf.o",
+    //       url: "https://web.archive.org/web/*/letsjustbefriends.org",
+		// 			description: " ",
+		// 			tags: [ ],
+    // year: "2004-2009",
+		// 			type: " ",
+		// 			status: " ",
+		// 		},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// 		{
+		// 			title: "327.com",
+    //       url: "https://web.archive.org/web/*/327.com",
+		// 			description: "The site that started it all. An occasionally-updated web-zine w/ album reviews, photo features, editorials, interviews...really just whatever we felt like. It was also home to web sites for several of my friends. I sold the domain in 2009 and still regret that decision to this day ¯\_(ツ)_/¯",
+		// 			tags: [ ],
+    // year: "1998-2009",
+		// 			type: " ",
+		// 			status: " ",
+		// 		},
+
+
+
+
+
+
+
+
+];
